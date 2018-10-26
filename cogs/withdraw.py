2800b1d39d5e054f0feffcdb3fd98a7d8548a639
Sotiris Blad <sotiris2009@hotmail.com>
2018-10-26 22:49:34 +0300

    Update withdraw.py

--- cogs/withdraw.py
+++ cogs/withdraw.py
@@ -13,7 +13,7 @@ class Withdraw:
 
     @commands.command(pass_context=True)
     async def withdraw(self, ctx, address: str, amount: float):
-        """Withdraw coins from your account to any Bitcoin with MonetaryUnit address"""
+        """Withdraw coins from your account to any MonetaryUnit address"""
         snowflake = ctx.message.author.id    
         if amount <= 0.0:
             await self.bot.say("{} **:warning: You cannot withdraw <= 0! :warning:**".format(ctx.message.author.mention))
